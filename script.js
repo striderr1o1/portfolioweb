58c372d112d285f0b6a79605905c42ad67a51195
Ranger <mnipk1243@gmail.com>
2025-01-11 21:49:41 +0500

    js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let headernote = document.getElementById("headnote");
-
-let text="cout << 'portfolio';";
-
-for(let i=0;i<text.length;i++)
-{
-    headernote.textContent+=text[i];
+function changecover(){
+    let getimage = document.getElementById("covercont");
     
-}
+
+    getimage.addEventListener("mouseover", function () {
+            getimage.style.backgroundImage="url('WhatsApp Image 2025-01-11 at 20.54.37_aaa9bca4.jpg')";
+            getimage.style.transition="all 1s"  
+    })
+    getimage.addEventListener("mouseout", function () {
+        getimage.style.backgroundImage="url('WhatsApp Image 2024-12-27 at 23.31.19_5a5e66e1.jpg')";
+        getimage.style.transition="all 1s"  
+})
+};
+
+changecover();
 
   

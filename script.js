4b97010c6acf1e4b3222b44778d17229d26b83ef
Ranger <mnipk1243@gmail.com>
2025-01-13 14:58:11 +0500

    added links

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function changecover(){
 
     getimage.addEventListener("mouseover", function () {
             getimage.style.backgroundImage="url('WhatsApp Image 2025-01-11 at 20.54.37_aaa9bca4.jpg')";
-            getimage.style.transition="all 1s"  
+            getimage.style.transition="all 1s";
     })
     getimage.addEventListener("mouseout", function () {
         getimage.style.backgroundImage="url('WhatsApp Image 2024-12-27 at 23.31.19_5a5e66e1.jpg')";
-        getimage.style.transition="all 1s"  
+        getimage.style.transition="all 1s";
 })
 };
+
+
 
 changecover();
 
